bf9924f0945fed71b825c0433f216fd072c502df
Jasmeet155061 <30699124+Jasmeet155061@users.noreply.github.com>
2017-08-14 14:19:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,9 +26,8 @@ function createTemplate(data) {
     var heading = data.heading;
     var content = data.content;
 } 
-var htmlTemplate = `
-
-<html>
+var htmlTemplate = 
+`<html>
     <head>
         <title>
         ${title}
@@ -55,7 +54,7 @@ var htmlTemplate = `
     </body>
 </html>
 `;
-return htmlTemplate;
+ return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
